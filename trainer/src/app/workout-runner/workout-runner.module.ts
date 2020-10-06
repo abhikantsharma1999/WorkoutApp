6f147fc15daabe1fc913ece899203fad1ea27e0d
Abhikant Sharma <33572006+abhikantsharma1999@users.noreply.github.com>
2020-10-06 19:29:25 +0530

    Changes made

---
+++ trainer/src/app/workout-runner/workout-runner.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkoutRunnerComponent } from './workout-runner.component';
+import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { SecondsToTimePipe } from './shared/seconds-to-time.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    WorkoutRunnerComponent
+  ],
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent, SecondsToTimePipe]
+})
+export class WorkoutRunnerModule { }
